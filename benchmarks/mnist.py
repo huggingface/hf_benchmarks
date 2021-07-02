e77a975c7cef767f4871cdc7ee7c33a67802494d
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-07-02 19:08:31 +0200

    Add mnist example script for evaluation

---
+++ benchmarks/mnist.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset, load_metric
+
+def evaluate(test_dataset, submission_dataset, task, **kwargs):
+    test_ds = load_dataset(test_dataset)
+    preds_ds = load_dataset(submission_dataset, task)
+    acc = load_metric("accuracy")
+    metrics = {}
+    for split in test_ds.keys():
+        metrics[split] = acc.compute(predictions=preds_ds[split]["preds"], references=test_ds[split]["label"])
+
+    return metrics
+
+
