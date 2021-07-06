8ed262d3a164a52f7d07f3e9102409e310ea7eeb
abhishek thakur <1183441+abhishekkrthakur@users.noreply.github.com>
2021-07-06 13:02:56 +0200

    use_auth_token

--- benchmarks/mnist/evaluation.py
+++ benchmarks/mnist/evaluation.py
@@ -8,13 +8,14 @@ from datasets import load_dataset, load_metric
 def evaluate(
     test_dataset: str = "mnist",
     submission_dataset: str = "lewtun/mnist-preds",
+    use_auth_token: bool = False,
     **kwargs
 ) -> Dict[str, Dict[str, Dict[str, float]]]:
     metrics = defaultdict(dict)
     tasks = ["task1", "task2"]
-    test_ds = load_dataset(test_dataset)
+    test_ds = load_dataset(test_dataset, use_auth_token=use_auth_token)
     for task in tasks:
-        preds_ds = load_dataset(submission_dataset, task)
+        preds_ds = load_dataset(submission_dataset, task, use_auth_token=use_auth_token)
         acc = load_metric("accuracy")
         for split in test_ds.keys():
             metrics[task][split] = acc.compute(
