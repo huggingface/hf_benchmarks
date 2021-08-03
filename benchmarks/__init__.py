003fbb8813b2e83ce85b820636c7d3107e22861f
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-08-02 20:48:37 -0500

    Add benchmark registry

---
+++ benchmarks/__init__.py
@@ -0,0 +1,9 @@
+from .registration import Benchmark, registry
+
+superb = Benchmark(name="superb")
+raft = Benchmark(name="raft")
+common = Benchmark(name="common")
+
+registry.register_benchmark(superb)
+registry.register_benchmark(raft)
+registry.register_benchmark(common)
