50c4654efc70768e6d2991b9a7b76d98dbf50bce
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-08-25 10:14:50 +0200

    Use real dataset and dummy predictions

--- benchmarks/raft/evaluation.py
+++ benchmarks/raft/evaluation.py
@@ -1,5 +1,5 @@
 import numpy as np
-from datasets import load_dataset, load_metric
+from datasets import get_dataset_config_names, load_dataset, load_metric
 
 from evaluate import Evaluation, Metric, Result, Task, get_benchmark_tasks
 
@@ -21,36 +21,29 @@ def evaluate(evaluation_dataset: str, submission_dataset: str, use_auth_token: s
     """
 
     # If your benchmark has multiple tasks, define their names here
-    tasks = get_benchmark_tasks(evaluation_dataset)
+    tasks = get_dataset_config_names(evaluation_dataset)
     # Load metric
-    # TODO(lewtun): add more metrics!
     f1 = load_metric("f1")
     # Define container to store metrics
     evaluation = Evaluation(results=[])
     # Iterate over tasks and build up metrics
     for task in tasks:
         task_data = Task(name=task, type="text-classification", metrics=[])
-        if task == "TAISafety-binary":
+        if task == "gpai_initiatives":
+            # TODO(lewtun): Figure out how to handle the label column for this task
+            task_data["metrics"].append(Metric(name="f1", type="f1", value=np.random.random()))
+        else:
             # Load datasets associated with task
             evaluation_ds = load_dataset(path=evaluation_dataset, name=task, use_auth_token=use_auth_token)
             submission_ds = load_dataset(path=submission_dataset, name=task, use_auth_token=use_auth_token)
-            # Sort by title for alignment
-            evaluation_ds = evaluation_ds.sort("title")
-            submission_ds = submission_ds.sort("title")
-            # Create label IDs
-            # TODO(lewtun): use ClassLabel type on Dataset side to skip this
-            evaluation_ds = evaluation_ds.map(convert_labels_to_ids)
-            submission_ds = submission_ds.map(convert_labels_to_ids)
-            # Compute metrics and build up list of dictionaries, one per task in your benchmark
+            # Compute metrics and build up list of dictionaries, one per task in the benchmark
             scores = f1.compute(
-                predictions=submission_ds["test"]["label"],
-                references=evaluation_ds["test"]["label"],
-                average="binary",
+                predictions=submission_ds["test"]["Label"],
+                references=evaluation_ds["test"]["Label"],
+                average="macro",
             )
             for k, v in scores.items():
                 task_data["metrics"].append(Metric(name=k, type=k, value=np.random.random()))
-        else:
-            task_data["metrics"].append(Metric(name="f1", type="f1", value=np.random.random()))
         # Collect results
         result = Result(task=task_data)
         evaluation["results"].append(result)
