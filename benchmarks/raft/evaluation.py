5e090baa4307eca2ed5003ee31bf91d672b58aca
lewtun <lewis.c.tunstall@gmail.com>
2021-08-27 14:04:22 +0200

    Update evaluation script (#39)
    * Refactor evaluation run
    
    * Add TODOs
    
    * Add TODO

--- benchmarks/raft/evaluation.py
+++ benchmarks/raft/evaluation.py
@@ -29,21 +29,18 @@ def evaluate(evaluation_dataset: str, submission_dataset: str, use_auth_token: s
     # Iterate over tasks and build up metrics
     for task in tasks:
         task_data = Task(name=task, type="text-classification", metrics=[])
-        if task == "gpai_initiatives":
-            # TODO(lewtun): Figure out how to handle the label column for this task
-            task_data["metrics"].append(Metric(name="f1", type="f1", value=np.random.random()))
-        else:
-            # Load datasets associated with task
-            evaluation_ds = load_dataset(path=evaluation_dataset, name=task, use_auth_token=use_auth_token)
-            submission_ds = load_dataset(path=submission_dataset, name=task, use_auth_token=use_auth_token)
-            # Compute metrics and build up list of dictionaries, one per task in the benchmark
-            scores = f1.compute(
-                predictions=submission_ds["test"]["Label"],
-                references=evaluation_ds["test"]["Label"],
-                average="macro",
-            )
-            for k, v in scores.items():
-                task_data["metrics"].append(Metric(name=k, type=k, value=np.random.random()))
+        # Load datasets associated with task
+        # TODO(lewtun): select test split and sort by IDs - need to convert to int first!
+        evaluation_ds = load_dataset(path=evaluation_dataset, name=task, use_auth_token=use_auth_token)
+        submission_ds = load_dataset(path=submission_dataset, name=task, use_auth_token=use_auth_token)
+        # Compute metrics and build up list of dictionaries, one per task in the benchmark
+        scores = f1.compute(
+            predictions=submission_ds["test"]["Label"],
+            references=evaluation_ds["test"]["Label"],
+            average="macro",
+        )
+        for k, v in scores.items():
+            task_data["metrics"].append(Metric(name=k, type=k, value=np.random.random()))
         # Collect results
         result = Result(task=task_data)
         evaluation["results"].append(result)
