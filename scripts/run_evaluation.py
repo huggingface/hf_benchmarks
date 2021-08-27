0f18e30e447c661c7a87cc4b4c88bd0add48ee56
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-08-27 11:39:44 +0200

    Refactor evaluation run

--- scripts/run_evaluation.py
+++ scripts/run_evaluation.py
@@ -3,6 +3,7 @@ import re
 import subprocess
 from pathlib import Path
 
+import pandas as pd
 import requests
 import typer
 from dotenv import load_dotenv
@@ -11,19 +12,28 @@ from dotenv import load_dotenv
 if Path(".env").is_file():
     load_dotenv(".env")
 
+auth_token = os.getenv("HF_HUB_TOKEN")
+header = {"Authorization": "Bearer " + auth_token}
+
 app = typer.Typer()
 
 
+def extract_tags(dataset):
+    tags = {}
+    for tag in dataset["tags"]:
+        k, v = tuple(tag.split(":", 1))
+        tags[k] = v
+    return tags
+
+
 def get_submission_repos(benchmark: str):
-    auth_token = os.getenv("HF_HUB_TOKEN")
-    header = {"Authorization": "Bearer " + auth_token}
     response = requests.get("http://huggingface.co/api/datasets", headers=header)
     all_datasets = response.json()
     submissions = []
 
     for dataset in all_datasets:
-        is_benchmark = any([t for t in dataset["tags"] if t.split(":")[1] == f"{benchmark}"])
-        if is_benchmark:
+        tags = extract_tags(dataset)
+        if tags.get("benchmark") == benchmark and tags.get("type") == "prediction-upload":
             submissions.append(dataset)
 
     return submissions
@@ -36,6 +46,16 @@ def run(benchmark: str, evaluation_dataset: str):
 
     for submission in submissions:
         submission_dataset = submission["id"]
+        response = requests.get(
+            f"http://huggingface.co/api/datasets/{submission_dataset}?full=true",
+            headers=header,
+        )
+        data = response.json()
+        # Extract submission timestamp and convert to Unix epoch in nanoseconds
+        timestamp = pd.to_datetime(data["lastModified"])
+        submission_timestamp = int(timestamp.timestamp() * 10 ** 9)
+        # Use the Git commit SHA and timestamp to create submission ID
+        submission_id = data["sha"] + "-" + str(submission_timestamp)
         process = subprocess.run(
             [
                 "autonlp",
@@ -46,18 +66,19 @@ def run(benchmark: str, evaluation_dataset: str):
                 f"{evaluation_dataset}",
                 "--submission",
                 f"{submission_dataset}",
+                "--submission_id",
+                f"{submission_id}",
             ],
             stdout=subprocess.PIPE,
         )
         if process.returncode == -1:
             typer.echo(f"Error launching evaluation job for submission {submission_dataset} on {benchmark} benchmark!")
         else:
-            match_job_id = re.search(r"# (\d+)", process.stdout.decode("utf-8"))
-            job_id = match_job_id.group(1)
-            typer.echo(
-                f"Successfully launched evaluation job #{job_id} for submission {submission_dataset} on {benchmark} benchmark!"
-            )
-
-
-if __name__ == "__main__":
-    app()
+            try:
+                match_job_id = re.search(r"# (\d+)", process.stdout.decode("utf-8"))
+                job_id = match_job_id.group(1)
+                typer.echo(
+                    f"Successfully launched evaluation job #{job_id} for submission {submission_dataset} on {benchmark} benchmark!"
+                )
+            except Exception as e:
+                typer.echo(f"Could not extract AutoNLP job ID due to error: {e}")
