bc85ea42e75197d8183a9101b3b1017f88a233eb
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-09-17 11:52:01 +0200

    Unit test get_benchmark_repos

--- tests/test_hub.py
+++ tests/test_hub.py
@@ -1,6 +1,16 @@
 from unittest import TestCase
 
-from evaluate.hub import extract_tags
+from huggingface_hub import HfFolder
+
+from evaluate import extract_tags, get_benchmark_repos
+
+from .testing_utils import (
+    BOGUS_BENCHMARK_NAME,
+    DUMMY_BENCHMARK_NAME,
+    DUMMY_EVALUATION_ID,
+    DUMMY_MODEL_ID,
+    DUMMY_PREDICTION_ID,
+)
 
 
 class ExtractTagsTest(TestCase):
@@ -28,3 +38,45 @@ class ExtractTagsTest(TestCase):
         repo_info = {"modelId": "bert-base-uncased", "tags": ["exbert", "benchmark:glue", "dataset:wikipedia"]}
         tags = extract_tags(repo_info)
         self.assertDictEqual(tags, {"benchmark": "glue", "dataset": "wikipedia"})
+
+
+class GetBenchmarkReposTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Share this valid token in all tests below.
+        """
+        cls._token = HfFolder.get_token()
+
+    def test_no_datasets_repo(self):
+        data = get_benchmark_repos(
+            benchmark=BOGUS_BENCHMARK_NAME, use_auth_token=True, endpoint="datasets", repo_type="prediction"
+        )
+        self.assertEqual(len(data), 0)
+
+    def test_no_models_repo(self):
+        data = get_benchmark_repos(
+            benchmark=BOGUS_BENCHMARK_NAME, use_auth_token=True, endpoint="models", repo_type="prediction"
+        )
+        self.assertEqual(len(data), 0)
+
+    def test_prediction_repo(self):
+        data = get_benchmark_repos(
+            benchmark=DUMMY_BENCHMARK_NAME, use_auth_token=True, endpoint="datasets", repo_type="prediction"
+        )
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["id"], DUMMY_PREDICTION_ID)
+
+    def test_evaluation_repo(self):
+        data = get_benchmark_repos(
+            benchmark=DUMMY_BENCHMARK_NAME, use_auth_token=True, endpoint="datasets", repo_type="evaluation"
+        )
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["id"], DUMMY_EVALUATION_ID)
+
+    def test_model_upload_repo(self):
+        data = get_benchmark_repos(
+            benchmark=DUMMY_BENCHMARK_NAME, use_auth_token=True, endpoint="models", repo_type="model"
+        )
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["modelId"], DUMMY_MODEL_ID)
