0afe4acd798f528daa263c99750b476dd7d64357
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-09-17 12:41:12 +0200

    WIP: Add time window for submissions

--- src/evaluate/hub.py
+++ src/evaluate/hub.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Dict, List, Union
 
 import requests
@@ -38,6 +39,8 @@ def get_benchmark_repos(
     use_auth_token: Union[bool, str, None] = None,
     endpoint: str = "datasets",
     repo_type: str = "prediction",
+    submission_window_start: str = None,
+    submission_window_end: str = None,
 ) -> List[Dict]:
     """Gets the metadata associated with benchmark submission and evaluation repositories.
 
@@ -46,6 +49,8 @@ def get_benchmark_repos(
         auth_token: The authentication token for the Hugging Face Hub
         endpoint: The endpoint to query. Can be `datasets` or `models`.
         repo_type: The type of benchmark repository. Can be `prediction`, `model` or `evaluation`.
+        submission_window_start: The timestamp for the start of the submission window.
+        submission_window_end: The timestamp for the end of the submission window.
 
     Returns:
         The benchmark repositories' metadata of a given `repo_type`.
@@ -57,6 +62,7 @@ def get_benchmark_repos(
         if token is None:
             raise EnvironmentError("You specified use_auth_token=True, but a huggingface token was not found.")
         headers = {"Authorization": f"Bearer {token}"}
+    # Fetch all metadata to access lastModified etc
     params = {"full": True}
     response = requests.get(f"http://huggingface.co/api/{endpoint}/", headers=headers, params=params)
     response.raise_for_status()
@@ -64,6 +70,18 @@ def get_benchmark_repos(
     submissions = []
 
     for repo in repos:
+        # Check whether submission falls within submission window
+        if submission_window_start and submission_window_end:
+            window_start = datetime.strptime(submission_window_start, "%Y-%m-%d")
+            window_end = datetime.strptime(submission_window_end, "%Y-%m-%d")
+            last_modified = repo.get("lastModified")
+            if last_modified:
+                timestamp = datetime.strptime(last_modified, "%Y-%m-%dT%H:%M:%S.%fZ")
+                if timestamp >= window_start and timestamp <= window_end:
+                    pass
+            else:
+                continue
+
         tags = extract_tags(repo)
         # Filter submission templates which have the submission_name="none" default value
         if (
