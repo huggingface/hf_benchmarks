eb99a47f7cc87cc0994c09a00c04d7fc23afe7c8
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-09-17 16:25:05 +0200

    Add submission table script

---
+++ scripts/submission_table.py
@@ -0,0 +1,53 @@
+import os
+from pathlib import Path
+
+import pandas as pd
+import typer
+from dotenv import load_dotenv
+
+from evaluate import get_benchmark_repos
+
+
+if Path(".env").is_file():
+    load_dotenv(".env")
+
+auth_token = os.getenv("HF_HUB_TOKEN")
+
+app = typer.Typer()
+
+
+@app.command()
+def run(
+    benchmark: str,
+    endpoint: str = "datasets",
+    repo_type: str = "prediction",
+    start_date: str = None,
+    end_date: str = None,
+    save_path: str = ".",
+):
+    if start_date is None or end_date is None:
+        default_start_time = pd.Timestamp.now()
+        default_end_time = pd.Timestamp.now() - pd.Timedelta(days=7)
+        typer.echo(
+            f"Submission window not provided, so using past week from {default_start_time.date()} as default window"
+        )
+        start_date = str(default_start_time.date())
+        end_date = str(default_end_time.date())
+
+    submissions = get_benchmark_repos(
+        benchmark=benchmark,
+        use_auth_token=True,
+        endpoint=endpoint,
+        repo_type=repo_type,
+        start_date=start_date,
+        end_date=end_date,
+    )
+    typer.echo(f"Found {len(submissions)} submissions for evaluation!")
+    df = pd.DataFrame(submissions)
+    file_path = os.path.join(save_path, f"{benchmark}_submissions_{start_date}_{end_date}.csv")
+    df.to_csv(file_path, index=False)
+    typer.echo(f"Saved submissions to {os.path.abspath(file_path)}")
+
+
+if __name__ == "__main__":
+    app()
