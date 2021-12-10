0c4cbc6cab8ebaf6a1735a060e3fa48f3a57e6ad
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2021-12-10 10:32:04 +0100

    Refactor compute_metrics() to use AutoNLP versions of huggingface_hub

--- benchmarks/gem/evaluation.py
+++ benchmarks/gem/evaluation.py
@@ -1,7 +1,7 @@
 import json
 import subprocess
 
-from huggingface_hub import cached_download, hf_hub_url, list_repo_files # type: ignore
+from huggingface_hub import cached_download, hf_hub_url # type: ignore
 
 
 def compute_metrics(evaluation_dataset: str, submission_dataset: str, use_auth_token: str) -> dict:
@@ -13,18 +13,18 @@ def compute_metrics(evaluation_dataset: str, submission_dataset: str, use_auth_t
         use_auth_token (:obj:`str`): The API token to access your private dataset on the Hugging Face Hub.
 
     Returns:
-        evaluation (:obj:`dict`): The evaluation metrics.
+        evaluation (:obj:`Evaluation`): The evaluation metrics.
     """
     metrics_filename = "metrics.json"
+    # This assumes that the GEM submissions are a single file, with a predefined name
+    # We'll need to enforce this on the submission repositories
+    submission_filename = "submission.json"
 
-    predictions_files = list_repo_files(submission_dataset, repo_type="dataset", token=use_auth_token)
-    # TODO(lewtun): Add tests to catch exceptions with multiple files
-    predictions_filename = [f for f in predictions_files if ".json" in f][0]
-    predictions_url = hf_hub_url(submission_dataset, predictions_filename, repo_type="dataset")
-    predictions_filepath = cached_download(predictions_url, use_auth_token=use_auth_token)
-
+    submission_url = hf_hub_url(submission_dataset, submission_filename, repo_type="dataset")
+    submission_filepath = cached_download(submission_url, use_auth_token=use_auth_token)
+    # gem_metrics automatically downloads the evaluation splits from the Hub
     process = subprocess.run(
-        ["gem_metrics", f"{predictions_filepath}", "-o", f"{metrics_filename}"],
+        ["gem_metrics", f"{submission_filepath}", "-o", f"{metrics_filename}"],
         stdout=subprocess.PIPE,
     )
     if process.returncode == -1:
@@ -33,5 +33,4 @@ def compute_metrics(evaluation_dataset: str, submission_dataset: str, use_auth_t
         with open(metrics_filename, "r") as f:
             metrics = json.load(f)
 
-    # TODO(lewtun): Test YAML conversion is OK in backend / dataset card
     return metrics
