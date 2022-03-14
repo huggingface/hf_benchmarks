784d619c52a66105d1d099ed14000122f2e989f1
lewtun <lewis.c.tunstall@gmail.com>
2022-03-14 11:56:25 +0100

    Push GEM outputs and scores (#60)

--- src/evaluate/file_utils.py
+++ src/evaluate/file_utils.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 
 def load_json(path):
@@ -7,5 +8,6 @@ def load_json(path):
 
 
 def save_json(path, data):
+    Path(path).parent.mkdir(parents=True, exist_ok=True)
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f)
